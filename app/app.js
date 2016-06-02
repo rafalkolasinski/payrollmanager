a86e04b1b60d7619b6a697281769371e611cd10a
Rafal Kolasinski <rafal.kolasinski92@gmail.com>
2016-06-02 23:49:23 +0200

    Added angular-localForage data handling

--- app/app.js
+++ app/app.js
@@ -3,8 +3,19 @@
 angular.module('PayrollManager', [
   'ionic',
   'ngCordova',
+  'LocalForageModule'
   // TODO: load other modules selected during generation
 ])
+
+	.config(['$localForageProvider', function($localForageProvider) {
+		$localForageProvider.config({
+			driver: 'localStorageWrapper',
+			name: 'pm',
+			version: 1.0,
+			storeName: 'employees',
+			description: 'Employees DB with personal data and recent payrolls.'
+		});
+	}])
 
 	.run(function ($ionicPlatform) {
 		$ionicPlatform.ready(function () {
